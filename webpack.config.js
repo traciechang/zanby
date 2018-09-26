e3ae73a2cf0102d79163013c16599e91a6efc583
Tracie <traciechang@hotmail.com>
2018-09-26 15:58:22 -0700

    setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/zanby.jsx",
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          plugins: ['transform-class-properties'],
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
