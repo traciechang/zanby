3147a1eeda94ab3a68639c8254449bb059de2557
Tracie <traciechang@hotmail.com>
2018-10-13 14:43:15 -0700

    new root route, home controller, home view

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,7 +2,7 @@ class Api::SessionsController < ApplicationController
     def create
         @user = User.find_or_create_from_auth_hash(request.env["omniauth.auth"])
         login(@user)
-        redirect_to "/"
+        redirect_to home_url
     end
 
     def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  root 'static_pages#root'
+  root 'sessions#new'
 
   get 'auth/:provider/callback', to: 'api/sessions#create'
   get 'auth/failure', to: redirect('/')
+  
+  resources :sessions, only: [:new]
+  resource :home, only: [:show] # get '/home', to: 'home#show'
 
   namespace :api, defaults: { format: :json } do
     resource :session, only: [:create, :destroy]
