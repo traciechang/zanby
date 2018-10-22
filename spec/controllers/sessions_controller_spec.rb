a2bc46bb2cdc75e87abf8ce54d0201c03e318070
Tracie <traciechang@hotmail.com>
2018-10-21 19:07:58 -0700

    write POST create specs for sessions controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe SessionsController, type: :controller do
     end
 
     describe 'POST create', type: :request do
-        it "makes a post request" do
+        before do
             OmniAuth.config.test_mode = true
 
             OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
@@ -26,9 +26,30 @@ RSpec.describe SessionsController, type: :controller do
                     email: 'pusheen@pusheencorp.com'
                 }
             })
-
+        end
+        
+        it "makes a post request" do
             get '/auth/google_oauth2/callback'
             expect(response.status).to eq(302)
+        end
+
+        it "creates a user" do
+            expect(User.find_by_email('pusheen@pusheencorp.com')).to eq(nil)
+
+            get '/auth/google_oauth2/callback'
+
+            expect(User.find_by_email('pusheen@pusheencorp.com')).to_not eq(nil)
+        end
+
+        it "logs the user in" do
+            expect(session[:user_id]).to eq(nil)
+            get '/auth/google_oauth2/callback'
+            expect(session[:user_id]).to_not eq(nil)
+        end
+
+        it "redirects to root after login" do
+            get '/auth/google_oauth2/callback'
+            expect(response).to redirect_to(root_url)
         end
     end
 
